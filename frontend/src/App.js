7e44fa8b9e6e61f5654dcd18822dc68fa9cc1d58
Sasindu1997 <sasinduusgalhewa@gmail.com>
2021-12-06 10:22:02 +0530

    app.js implementations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import button from"@material-ui/core/button"
+import iconButton from"@material-ui/core/iconButton"
+import TextField from"@material-ui/core/TextField"
+import AssignmentIcon from"@material-ui/core/AssignmentIcon"
+import phoneIcon from"@material-ui/core/phoneIcon"
+import React, { useState, useRef, useEffect} from "react"
+import copyToClipboard from "./copyToClipboard"
+import Peer from "simple-peer"
+import io from "socket.io-client"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
