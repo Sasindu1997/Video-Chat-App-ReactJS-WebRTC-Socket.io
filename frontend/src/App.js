e9c81d095d36e0a3c11b7244f13b4187c49eba6b
Sasindu1997 <sasinduusgalhewa@gmail.com>
2021-12-13 01:26:24 +0530

    Implement functions to UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-// import Button from '@mui/material/Button';
-// import IconButton from '@mui/material/IconButton';
-// import TextField from"@mui/material/TextField"
-// import AssignmentIcon from '@mui/icons-material/Assignment';
-// import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from"@material-ui/core/TextField"
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import LocalPhoneIcon from '@material-ui/icons/LocalPhone';
 import React, { useState, useRef, useEffect} from "react"
-import copyToClipboard from 'react-copy-to-clipboard'
+import CopyToClipboard from 'react-copy-to-clipboard'
 import Peer from "simple-peer"
 import io from "socket.io-client"
 import './App.css';
@@ -44,35 +44,38 @@ function App() {
       setCallerSignal(data.signal)
     })
 
-    const callUser = (id) => {
+    
+  }, [])
 
-      const peer = new Peer({
-        initiator: true, 
-        trickle: false, 
-        stream: stream
+  const callUser = (id) => {
+
+    const peer = new Peer({
+      initiator: true, 
+      trickle: false, 
+      stream: stream
+    })
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id, 
+        signal: data,
+        from: me,
+        name: name
       })
+    })
 
-      peer.on("signal", (data) => {
-        socket.emit("callUser", {
-          userToCall: id, 
-          signal: data,
-          from: me,
-          name: name
-        })
-      })
+    peer.on("stream", (data) => {
+      userVideo.current.srcObject = stream
+    })
 
-      peer.on("stream", (data) => {
-        userVideo.current.srcObject = stream
-      })
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true)
+      peer.signal(signal)
+    })
 
-      socket.on("callAccepted", (signal) => {
-        setCallAccepted(true)
-        peer.signal(signal)
-      })
+    connectionRef.current = peer
 
-      connectionRef.current = peer
-
-    }
+  }
 
     const answerCall = () => {
       setCallAccepted(true)
@@ -95,7 +98,6 @@ function App() {
     
     }
 
-  }, [])
 
   const leaveCall = () => {
     setCallEnded(true)
@@ -103,8 +105,61 @@ function App() {
   }
 
   return (
-    <div className="App">
-      
+    <div>
+      <h1 style={{ textAlign: "center", color: "fff" }}>VideoApp</h1>
+      <div className="container">
+        <div className="video-container">
+          <div className="video">
+            {stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "380px"}} />}
+          </div>
+          <div container="video">
+            {callAccepted && !callEnded ? 
+            <video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} /> : null }
+          </div>
+        </div>
+        <div className="myId">
+          <TextField
+            id="filled-basic"
+            label="Name"
+            varient="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{marginBottom: "20px"}}
+          />
+          <CopyToClipboard text={me} style={{ marginBottom: "2rem"}}>
+            <Button varient="contained" color="primary" startIcon={<AssignmentIcon fontSize="large"/>}>
+              Copy ID
+            </Button>
+          </CopyToClipboard>
+          <TextField 
+            id="filled-basic"
+            label="ID to Call"
+            varient="filled"
+            value={idToCall}
+            onChange={(e) => setIdToCall(e.target.value)}
+          />
+          <div className="call-button">
+              {callAccepted && !callEnded ? (
+                <Button varient="contained" color="secondary" onClick={leaveCall}>
+                  End Call
+                </Button>
+              ) : (
+                <IconButton color="primary" onClick={() => callUser(idToCall)} area-label="call">
+                  <LocalPhoneIcon fontSize="large" />
+                </IconButton>
+              )}
+              {idToCall}
+          </div>
+        </div>
+        {receivingCall && !callAccepted ? (
+          <div className="caller">
+            <h1>{name} is calling...</h1>
+            <Button varient="contained" color="primary" onClick={answerCall}>
+              Answer
+            </Button>
+          </div>
+        ): null}
+      </div>
     </div>
   );
 }
